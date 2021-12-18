75bcf9d9d0ab6ba476a38ec0644ad07fa2b55353
Pawan <pk738500@gmail.com>
2021-12-18 18:54:35 +0530

    new updates

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -56,7 +56,11 @@ class HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("Calculator"),
+        title: Center(
+            child: new Text(
+          "Calculator",
+          style: TextStyle(fontSize: 25.0),
+        )),
       ),
       body: new Container(
         padding: const EdgeInsets.all(40.0),
