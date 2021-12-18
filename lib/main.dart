05d6368b4323c8e0c2ed32a084a2fc92b0c9f3fb
Pawan <pk738500@gmail.com>
2021-12-18 18:51:30 +0530

    debug mode banner removed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 //ignore_for_file: use_key_in_widget_constructors,unnecessary_new,prefer_const_constructors,avoid_unnecessary_containers
 import 'package:flutter/material.dart';
 import 'home_page.dart';
+
 void main() {
   runApp(new MyApp());
 }
@@ -9,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Calculator",
       theme: new ThemeData(primarySwatch: Colors.red),
       home: new HomePage(),
